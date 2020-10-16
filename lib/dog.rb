66aaa4e2b79c1b358b4eb76fb8825e76563f7915
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-16 02:59:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,5 +20,20 @@ end
 
 class Person
 
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job 
+  end 
+
+  def job 
+    @job 
+  end 
 
 end
